61fce9595d2a36d7eec336f8b3df9fb2c06aaf7b
Adrian Philipp <mail@adrian-philipp.com>
2016-07-06 16:21:46 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Wiki automation
+
+I use a wiki/todo list based on plain text files. To ease my life these text files
+are modified automatically by small scripts.
+
+## Todo History
+
+Copies completed todos in history files.
---
+++ todo-history.js
@@ -0,0 +1,61 @@
+const chokidar = require('chokidar');
+const tilde = require('tilde-expansion');
+const fs = require('fs');
+var dateFormat = require('dateformat');
+
+const wikiPath = '~/Library/Mobile\ Documents/27N4MQEA55~pro~writer/Documents/Wiki_Synced/';
+const historySuffix = '-History.txt';
+
+function findTodos(content) {
+    return content.match(/^.*-\ \[x\](.*\s*)$/mg, '') || [];
+}
+
+function removeTodos(todos, content) {
+    const replace = (content, todo) => content.replace(todo + '\n', '');
+
+    return todos.reduce(replace, content) || content;
+}
+
+function todoHistory(todos, today) {
+    return "## " + dateFormat(today, 'yyyy-mm-dd') + '\n'
+        +  todos.join('\n');
+}
+
+function readFile(file, callback) {
+    fs.readFile(file, 'utf8', (err, content) => {
+        if (err) {
+            console.log(err);
+            return callback('');
+        }
+        callback(content);
+    });
+}
+
+function writeFile(file, content) {
+    fs.writeFile(file, content, function(err) {
+        if (err) throw err;
+    });
+}
+
+function moveCheckedTodosToHistory(file) {
+    readFile(file, (content) => {
+        const todos = findTodos(content);
+        console.log(todos);
+        if (!todos || todos.length === 0) return;
+        writeFile(file, removeTodos(todos, content));
+
+        const historyFile = file.replace('.txt', historySuffix);
+        readFile(historyFile, (history) => {
+            writeFile(historyFile, todoHistory(todos, new Date()) + "\n\n" + history)
+        });
+    });
+}
+
+tilde(wikiPath + 'ToDo.txt', file => moveCheckedTodosToHistory(file));
+tilde(wikiPath + 'Terberg Midoffice.txt', file => moveCheckedTodosToHistory(file));
+
+// Watcher version
+//tilde(wikiPath, dir =>
+//  chokidar
+//    .watch(dir, { ignored: `/.*${historySuffix}$/`, ignoreInitial: true })
+//    .on('change', file => moveCheckedTodosToHistory(file)));
