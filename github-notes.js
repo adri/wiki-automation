6ac10820b76c174c1e906c85746cc9c8a394d48a
Adrian Philipp <mail@adrian-philipp.com>
2020-08-22 09:33:17 +0200

    Notes > Create folders if they don't exist

--- github-notes.js
+++ github-notes.js
@@ -9,10 +9,18 @@ const notesPath = process.env.NOTES_PATH;
 const simpleGit = require('simple-git')(notesPath);
 
 function copy(file, folder, callback) {
-  console.log('cp "' + file + '" ' + folder);
-  require('child_process').exec('cp "' + file + '" ' + folder, (err, stdout) => {
-    console.log(err, stdout);
-    callback(folder + path.basename(file))
+  // create folder if it doens't exist 
+  require('child_process').exec('mkdir -p "' + folder + '"', (err, stdout) => {
+    if (err) {
+      console.log(err, stdout);
+      return;
+    }
+
+    // copy file to folder
+    require('child_process').exec('cp "' + file + '" ' + folder, (err, stdout) => {
+      console.log(err, stdout);
+      callback(folder + path.basename(file))
+    });
   });
 }
 
@@ -20,12 +28,16 @@ function commit(file) {
   console.log('commit ' + file);
   simpleGit
     .add(file)
-    .commit('Autocommit from wiki', [file])
+    .commit('Autocommit from notes', [file])
     .push('origin', 'master');
 }
 
 function commitFile(file) {
-  copy(file, notesPath + '/', (file) => commit(file));
+  const relativeFolder = file
+    .replace(path.basename(file), '')
+    .replace(wikiPath, '');
+
+  copy(file, notesPath + '/' + relativeFolder, (file) => commit(file));
 }
 
 tilde(wikiPath, dir => {
